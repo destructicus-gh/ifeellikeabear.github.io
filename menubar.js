668283498ac7079349b64a1eee4313ea8d9cb65b
ryan anders <ryan.anders444@gmail.com>
2015-02-23 17:02:51 -0600

    this?

--- menubar.js
+++ menubar.js
@@ -3,22 +3,5 @@ $( function()
 {
 	$( '#nav li:has(ul)' ).doubleTapToGo();
 });
-var jqxhr = $.get( "menubar.txt", function() {
-  alert( "success" );
-})
-  .done(function() {
-    alert( "second success" );
-  })
-  .fail(function() {
-    alert( "error" );
-  })
-  .always(function() {
-    alert( "finished" );
-  });
- 
-// Perform other work here ...
-// Set another completion function for the request above
-jqxhr.always(function() {
-  alert( "second finished" );
-});
-$( ".menubar" ).append( jqxhr );
+
+$( "#menubar" ).load( "menubar.html" );
